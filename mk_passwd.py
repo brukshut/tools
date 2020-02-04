0c6f1352b7d2bbabdf79690a844c5703a6640230
Christian Gough <brukshut@gmail.com>
2020-02-04 11:36:15 -0500

    Simple python script to generate random passwords.

---
+++ mk_passwd.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+##
+## simple python script to generate random passwords
+##
+import argparse
+import random
+import string
+
+## main
+def randomString(length):
+  """Generate a random string of fixed length """
+  letters = f'{string.digits}{string.ascii_uppercase}{string.ascii_lowercase}{string.punctuation}'
+  return ''.join(random.choice(letters) for i in range(int(length)))
+
+def argue():
+  parser = argparse.ArgumentParser(description="Generate Secure Password")
+  parser.add_argument("-l", "--length", required=True, help="length of password")
+  args = parser.parse_args()
+  return args
+
+def main():
+  args = argue()
+  print(randomString(args.length))
+
+## end functions
+
+## main
+main()
+## end main
